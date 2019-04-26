60644173a0bf53eb3dc6c2aae51627482dfc8afd
isabelatc <isabela.tellechea@ug.uchile.cl>
2019-04-26 16:37:38 -0400

    Creation and implementation of Attack methods

--- src/cc3002/t1/Attack.java
+++ src/cc3002/t1/Attack.java
@@ -1,6 +1,8 @@
 package cc3002.t1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Attack implements IAttack {
 
@@ -18,6 +20,57 @@ public class Attack implements IAttack {
         this.lightningCost = lightningCost;
         this.psychicCost = psychicCost;
         this.waterCost = waterCost;
-
     }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBaseDamage() {
+        return this.baseDamage;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public List<Integer> getEnergyCosts() {
+        return new ArrayList<>(Arrays.asList(fightingCost, fireCost, grassCost, lightningCost, psychicCost, waterCost));
+    }
+
+    @Override
+    public int getFightingCost() {
+        return this.fightingCost;
+    }
+
+    @Override
+    public int getFireCost() {
+        return this.fireCost;
+    }
+
+    @Override
+    public int getGrassCost() {
+        return this.fireCost;
+    }
+
+    @Override
+    public int getLightningCost() {
+        return this.lightningCost;
+    }
+
+    @Override
+    public int getPsychicCost() {
+        return this.psychicCost;
+    }
+
+    @Override
+    public int getWaterCost() {
+        return this.waterCost;
+    }
+
+
 }
--- src/cc3002/t1/IAttack.java
+++ src/cc3002/t1/IAttack.java
@@ -1,4 +1,27 @@
 package cc3002.t1;
 
+import java.util.List;
+
 public interface IAttack {
+
+    String getName();
+
+    int getBaseDamage();
+
+    String getDescription();
+
+    List<Integer> getEnergyCosts();
+
+    int getFightingCost();
+
+    int getFireCost();
+
+    int getGrassCost();
+
+    int getLightningCost();
+
+    int getPsychicCost();
+
+    int getWaterCost();
+
 }
