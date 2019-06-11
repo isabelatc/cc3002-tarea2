47be4cb99ded97ee0e5559db459b59b93e1cb8e1
isabelatc <isabela.tellechea@ug.uchile.cl>
2019-06-11 01:00:56 -0400

    Update Attack Test

--- test/cc3002/t1/AttackTest.java
+++ test/cc3002/t1/AttackTest.java
@@ -35,6 +35,7 @@ public class AttackTest {
         differentEnergyCounter.setFightingEnergy(2);
         differentEnergyCounter.setFireEnergy(3);
         differentEnergyCounter.setWaterEnergy(4);
+        
     }
 
     @Test
